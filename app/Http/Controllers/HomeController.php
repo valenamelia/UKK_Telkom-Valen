1ae34f0c024c8659f8f92dbca390870800aa17cf
Almax221 <alfajri2056@gmail.com>
2024-02-26 14:31:40 +0700

    Add New

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\Foto; // Pastikan menggunakan model yang sesuai
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $photos = Foto::all(); // Mengambil semua foto dari database
+        return view('home', ['photos' => $photos]);
+    }
+}
+
+
